f1e3880ef59c1f1f44e8e27ca43fd843fdbe4d52
krishshyara <krishshyara25@gmail.com>
2025-01-29 10:41:09 +0530

    added Game model

--- Backend/controllers/gameController.js
+++ Backend/controllers/gameController.js
@@ -1,5 +1,5 @@
 const Game = require('../models/Game');
-const User = require('../models/User');
+const User = require('../models/userModel');
 
 // Controller to fetch all games
 const getAllGames = async (req, res) => {
---
+++ Backend/models/Game.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// Define schema for Game
+const gameSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+// Create Game model
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = Game;
